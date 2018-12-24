0f97b6339b43e9bfd362446dc0dd5fdaaf83a86b
lyle8341 <lyllyl0000@126.com>
2018-12-24 23:48:37 +0800

    优化模块

---
+++ miaosha8/src/main/java/com/lyle/ms/Miaosha8Application.java
@@ -0,0 +1,14 @@
+package com.lyle.ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Miaosha8Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Miaosha8Application.class, args);
+  }
+
+}
+
