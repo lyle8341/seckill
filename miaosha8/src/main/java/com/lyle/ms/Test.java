dd0bf01f2d24a2b0699d7463f1eb9753ea6e25d1
lyle8341 <lyllyl0000@126.com>
2018-12-26 22:56:20 +0800

    测试，对比

--- miaosha8/src/main/java/com/lyle/ms/Test.java
+++ miaosha8/src/main/java/com/lyle/ms/Test.java
@@ -14,6 +14,8 @@ public class Test {
 
   public static void main(String[] args) throws Exception {
 
+//    long start = System.nanoTime();
+    long start = System.currentTimeMillis();
     User1 u1 = new User1();
     u1.setAge(11);
     u1.setName("user1");
@@ -21,7 +23,8 @@ public class Test {
     //length=101
     byte[] bytes1 = JdkSerializerService.serializeObj(u1);
     User1 obj = JdkSerializerService.getObj(bytes1);
-
+//    long middle = System.nanoTime();
+    long middle = System.currentTimeMillis();
     User2 u2 = new User2();
     u2.setAge(22);
     u2.setName("user2");
@@ -29,6 +32,11 @@ public class Test {
     //length=14
     byte[] bytes2 = ProtostuffSerializerService.serializeObj(u2);
     User2 obj1 = ProtostuffSerializerService.getObj(bytes2);
-    System.out.println("============");
+//    long end = System.nanoTime();
+    long end = System.currentTimeMillis();
+//    System.out.println("jdk:" + (middle - start) / (1000 * 1000) + "ms");
+//    System.out.println("protostuff:" + (end - middle) / (1000 * 1000) + "ms");
+    System.out.println("jdk:" + (middle - start) + "ms");
+    System.out.println("protostuff:" + (end - middle) + "ms");
   }
 }
