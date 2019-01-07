83c6b6af37899c06155bfd4cce7ef8c78cb47d7a
lyle8341 <lyllyl0000@126.com>
2019-01-08 00:24:41 +0800

    add log

--- miaosha8/src/main/java/com/lyle/ms/Miaosha8Application.java
+++ miaosha8/src/main/java/com/lyle/ms/Miaosha8Application.java
@@ -1,9 +1,11 @@
 package com.lyle.ms;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = {"com.lyle.ms.dao"})
 public class Miaosha8Application {
 
   public static void main(String[] args) {
